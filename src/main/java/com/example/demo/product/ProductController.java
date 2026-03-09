babb1710f4d917f0985614c5a759c2c07de53088
Niharika <niharikarb191@gmail.com>
2026-03-09 11:45:21 +0530

    INitial commit -Devops Ecommerce project

---
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.demo.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
